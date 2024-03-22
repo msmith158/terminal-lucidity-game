e0de8e30945d694d6d299955060d761410b5e374
msmith158 <1026618@student.sae.edu.au>
2024-03-22 13:53:14 +0800

    Wheelchair can now move

--- Assets/Wheelchair.cs
+++ Assets/Wheelchair.cs
@@ -4,14 +4,22 @@ using UnityEngine;
 
 public class Wheelchair : MonoBehaviour
 {
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float moveSmoothAmount;
     [SerializeField] private float turnSpeed;
+
     private GameObject playerController;
     private Rigidbody rb;
+
+    private float realMoveSpeed;
+    private float realTurnSpeed;
+    private float timeElapsed;
 
     private void Start()
     {
         playerController = GameObject.Find("FPEPlayerController(Clone)");
         playerController.transform.parent = transform;
+        rb = GetComponent<Rigidbody>();
     }
 
     void Update()
@@ -22,7 +30,15 @@ public class Wheelchair : MonoBehaviour
             if (Input.GetKey(KeyCode.Q) && Input.GetKey(KeyCode.E))
             {
                 // Forward
-                print("Forward");
+                if (timeElapsed < moveSmoothAmount)
+                {
+                    rb.velocity = transform.forward * Mathf.Lerp(0, -moveSpeed, timeElapsed / moveSmoothAmount);
+                    timeElapsed += Time.deltaTime;
+                }
+                else
+                {
+                    rb.velocity = transform.forward * -moveSpeed;
+                }
             }
             else
             {
@@ -39,8 +55,16 @@ public class Wheelchair : MonoBehaviour
         {
             if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.D))
             {
-                // Backward
-                print("Backward");
+                // Backwards
+                if (timeElapsed < moveSmoothAmount)
+                {
+                    rb.velocity = transform.forward * Mathf.Lerp(0, moveSpeed, timeElapsed / moveSmoothAmount);
+                    timeElapsed += Time.deltaTime;
+                }
+                else
+                {
+                    rb.velocity = transform.forward * moveSpeed;
+                }
             }
             else
             {
@@ -52,6 +76,11 @@ public class Wheelchair : MonoBehaviour
         {
             // Left
             transform.Rotate(transform.up, Time.deltaTime * turnSpeed);
+        }
+
+        if (Input.GetKeyUp(KeyCode.Q) || Input.GetKeyUp(KeyCode.E) || Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D))
+        {
+            timeElapsed = 0;
         }
     }
 }
@@ -68,4 +97,16 @@ public class Wheelchair : MonoBehaviour
  *     
  * "How To Make An Object A Child Of Another Object (SCRIPT) - Questions & Answers - Unity Discussions"
  *     https://discussions.unity.com/t/how-to-make-an-object-a-child-of-another-object-script/94998
+ *     
+ * "Unity - Scripting API: Rigidbody.velocity"
+ *     https://docs.unity3d.com/ScriptReference/Rigidbody-velocity.html
+ *     
+ * "Velocity based on rotation - Unity Forum"
+ *     https://forum.unity.com/threads/velocity-based-on-rotation.32500/
+ *     
+ * "Unity - Scripting API: Rigidbody.constraints"
+ *     https://docs.unity3d.com/ScriptReference/Rigidbody-constraints.html
+ *     
+ * "The right way to Lerp in Unity (with examples) - Game Dev Beginner"
+ *     https://gamedevbeginner.com/the-right-way-to-lerp-in-unity-with-examples/
 */
